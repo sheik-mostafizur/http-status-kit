0b55962a75b86679a637787e0ec9f91cb15c82cc
sheik-mostafizur <sheikmostafizur@gmail.com>
2024-09-18 11:04:22 +0600

    working with build mode

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  sourcemap: true,
+  dts: true,
+  clean: true,
+  minify: false,    
+});
